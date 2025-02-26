e2ffe38cfc4f8244d4d25fc13a4f2dbbbaf0dad8
Wadii Douihech <wadiidouihech@gmail.com>
2025-02-26 16:11:53 +0100

    defined the actor class again with improved implementation

---
+++ actor.py
@@ -0,0 +1,37 @@
+from random import *
+
+class Actor:
+    def __init__(self, ip, name, port, intf, action):
+        self.ip = ip
+        self.name = name
+        self.port = port
+        self.intf = intf
+        self.action = action # an action object 
+        self.intfrole = "Undefined"
+        self.country = "Reserved"
+
+
+    # for wanting to defines the properties randomly
+    def ip_init(self):
+        self.ip = "192.168." + randint(0, 255) + "." + random(0, 255)
+    
+    def port_init(self):
+        self.port = randint(1000, 9999)
+
+    def intf_init(self):
+        self.intf = "port" + random(1,99)
+
+    # for the source
+    def start(self, action):
+        pass
+
+    def stop(self, action):
+        pass
+
+    # for the destination
+    def accept(self, action):
+        pass
+
+    def refuse(self, action):
+        pass
+    
